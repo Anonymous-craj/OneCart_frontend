32a23efc8eb3de8cc4a3b3916c4a69cd4e0f755b
Anonymous-craj <sirajbasnet5@gmail.com>
2025-06-19 07:00:35 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Route, Routes } from "react-router";
+import Registration from "./pages/Registration";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<Registration />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
